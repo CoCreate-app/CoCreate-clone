72a88f5d544dad4aa91a4f2aef8f33de5f3f7eda
frankpagan <frank@cocreate.app>
2021-01-27 15:04:35 +0000

    clean file tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-    socket: {
-        config: {
-            apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
-            securityKey: "f26baf68-e3a9-45fc-effe-502e47116265",
-            organization_Id: "5de0387b12e200ea63204d6c"
-        },
+    config: {
+        apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
+        securityKey: "f26baf68-e3a9-45fc-effe-502e47116265",
+        organization_Id: "5de0387b12e200ea63204d6c",
         host: "server.cocreate.app:8088"
     },
-
+ 
     sources: [{
             path: "./docs/index.html",
             collection: "website-pages",
