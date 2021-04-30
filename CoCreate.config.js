72f04effbf51fdea0835bba4cdf9b0d7153b72df
frank <frank@cocreate.app>
2021-04-30 04:00:28 +0000

    update CoCreate.config.js

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,47 +1,37 @@
 module.exports = {
-    config: {
-        apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
-        securityKey: "f26baf68-e3a9-45fc-effe-502e47116265",
-        organization_Id: "5de0387b12e200ea63204d6c",
-        host: "server.cocreate.app:8088"
-    },
- 
-    sources: [{
-            path: "./docs/index.html",
-            collection: "website-pages",
-            document_id: "60119d34ef30c40e45dde2da",
-            key: "html",
-            data:{
-                name:"Clone doc"
-            }
-        },
-    ],
+	"config": {
+		"apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
+		"securityKey": "f26baf68-e3a9-45fc-effe-502e47116265",
+		"organization_Id": "5de0387b12e200ea63204d6c",
+		"host": "server.cocreate.app:8088"
+	},
 
-    crud: [{
-            collection: "routes",
-            document_id: "60119d34ef30c40e45dde2d9",
-            data:{
-                collection: "website-pages",
-                document_id: "60119d34ef30c40e45dde2da",
-                domains: ["general.cocreate.app"],
-                name: "html",
-                route: "/docs/clone",
+    "sources": [{
+            "path": "./docs/index.html",
+            "collection": "files",
+            "document_id": "60119d34ef30c40e45dde2da",
+            "key": "html",
+            "data":{
+                "name": "clone Doc",
+                "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
+                "path": "/docs/clone"
             }
         }
     ],
-    
-    extract: {
-        directory: "./src/",
-        extensions: [
-            "js",
-            "css",
-            "html"
-        ],
-        ignores: [
-            "node_modules",
-            "vendor",
-            "bower_components",
-            "archive"
-        ],
-    }
+
+	"extract": {
+		"directory": "./src/",
+		"extensions": [
+			"js",
+			"css",
+			"html"
+		],
+		"ignores": [
+			"node_modules",
+			"vendor",
+			"bower_components",
+			"archive"
+		]
+	}
 }
+
